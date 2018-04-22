c76b521722a65b25e7a8eb5518642735d893aace
Daniel Stanciu <danielstanciu91@gmail.com>
2018-04-22 13:22:36 +0100

    Add constants for the player model

--- core/src/com/turbochargedapps/icicles/Constants.java
+++ core/src/com/turbochargedapps/icicles/Constants.java
@@ -15,4 +15,12 @@ public class Constants {
     public static final float ICICLE_HEIGHT = 1f;
     public static final float ICICLE_WIDTH = 0.5f;
     public static final Color ICICLE_COLOR = Color.WHITE;
+
+    //Player
+    public static final float PLAYER_HEAD_RADIUS = 0.5f;
+    public static final float PLAYER_HEAD_HEIGHT = 4.0f * PLAYER_HEAD_RADIUS;
+    public static final float PLAYER_HEAD_SEGMENTS = 20;
+    public static final float PLAYER_LIMB_WIDTH = 0.1f;
+    public static final Color PLAYER_COLOR = Color.BLACK;
+
 }
