7ed7d88cdf47cc8097adf4bdba065cf01bb4f83d
Daniel Stanciu <danielstanciu91@gmail.com>
2018-04-28 17:19:31 +0100

    Remove icicles once they're completely off the bottom of the screen in order to save memory

--- core/src/com/turbochargedapps/icicles/actors/IcicleRain.java
+++ core/src/com/turbochargedapps/icicles/actors/IcicleRain.java
@@ -3,13 +3,13 @@ package com.turbochargedapps.icicles.actors;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.DelayedRemovalArray;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import static com.turbochargedapps.icicles.Constants.*;
 
 public class IcicleRain {
-    private Array<Icicle> icicleRain;
+    private DelayedRemovalArray<Icicle> icicleRain;
     private Viewport viewport;
 
     public IcicleRain(Viewport viewport) {
@@ -21,7 +21,7 @@ public class IcicleRain {
      * Sets the initial position
      */
     public void init() {
-        icicleRain = new Array<Icicle>(false, 100);
+        icicleRain = new DelayedRemovalArray<Icicle>(false, 100);
     }
 
     /**
@@ -43,10 +43,24 @@ public class IcicleRain {
         for (Icicle icicle : icicleRain) {
             icicle.update(delta);
         }
+
+        //Begin removal session
+        icicleRain.begin();
+
+        //Remove icicles completely off the bottom of the screen
+        for (int i = 0; i < icicleRain.size; i++) {
+            if (icicleRain.get(i).getPosition().y < -ICICLE_HEIGHT) {
+                icicleRain.removeIndex(i);
+            }
+        }
+
+        //End removal session
+        icicleRain.end();
     }
 
     /**
      * Renders the icicle rain on screen
+     *
      * @param renderer ShapeRenderer object
      */
     public void render(ShapeRenderer renderer) {
