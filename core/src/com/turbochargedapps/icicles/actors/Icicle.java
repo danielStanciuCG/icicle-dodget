2553cbb1963e0c20b78e3ec7d86fdbf1e05883f6
Daniel Stanciu <danielstanciu91@gmail.com>
2018-04-28 13:04:48 +0100

    Make the Icicle class package-private (including constructor and all methods

--- core/src/com/turbochargedapps/icicles/actors/Icicle.java
+++ core/src/com/turbochargedapps/icicles/actors/Icicle.java
@@ -9,7 +9,7 @@ import static com.turbochargedapps.icicles.Constants.*;
 /**
  * Blueprint for a single icicle object.
  */
-public class Icicle {
+class Icicle {
     private Vector2 position;
     private Vector2 velocity;
 
@@ -17,7 +17,7 @@ public class Icicle {
      * Creates an icicle at a specific position.
      * @param position The required position.
      */
-    public Icicle(Vector2 position) {
+    Icicle(Vector2 position) {
         this.position = position;
         velocity = new Vector2();
     }
@@ -26,23 +26,19 @@ public class Icicle {
      * Draws a single icicle to screen
      * @param renderer Requires an instance of ShapeRenderer in order to accomplish the task.
      */
-    public void render(ShapeRenderer renderer) {
-        renderer.setColor(ICICLE_COLOR);
-        renderer.set(ShapeType.Filled);
-
+    void render(ShapeRenderer renderer) {
         renderer.triangle(
                 position.x, position.y,
                 position.x - ICICLE_WIDTH / 2,position.y + ICICLE_HEIGHT,
                 position.x + ICICLE_WIDTH / 2, position.y + ICICLE_HEIGHT
                 );
-        renderer.end();
     }
 
     /**
      * Updates the position and state of the icicle
      * @param delta Time since last frame
      */
-    public void update(float delta) {
+    void update(float delta) {
         velocity.mulAdd(ICICLES_ACCELERATION, delta);
         position.mulAdd(velocity, delta);
 
